44f19f77dc8cb1c285fcbe7064645d286515038c
Arunkumar83 <30962126+Arunkumar83@users.noreply.github.com>
2019-11-22 13:24:16 +0530

    Invoked search after button click

--- src/learn/StateWithAxios.js
+++ src/learn/StateWithAxios.js
@@ -4,11 +4,12 @@ import axios from "axios";
 function StateFetchData() {
   const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState("redux");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchdata = async () => {
       const result = await axios(
-        `https://hn.algolia.com/api/v1/search?query=${query}`
+        `https://hn.algolia.com/api/v1/search?query=${search}`
       );
       setData(result.data);
     };
@@ -17,7 +18,7 @@ function StateFetchData() {
     //   "https://hn.algolia.com/api/v1/search?query=redux"
     // );
     // setData(result.data);
-  }, [query]);
+  }, [search]);
 
   return (
     <Fragment>
@@ -26,6 +27,10 @@ function StateFetchData() {
         value={query}
         onChange={event => setQuery(event.target.value)}
       />
+      <button type="button" onClick={() => setSearch(query)}>
+        {" "}
+        Search{" "}
+      </button>
       <ul>
         {data.hits.map(item => (
           <li key={item.objectID}>
