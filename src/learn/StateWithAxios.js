264d5cec2112c2bc756bbf6e6d50b6d5ca16be7a
Arunkumar83 <30962126+Arunkumar83@users.noreply.github.com>
2019-11-22 13:13:48 +0530

    Added query in useEffect

--- src/learn/StateWithAxios.js
+++ src/learn/StateWithAxios.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 
 function StateFetchData() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux");
 
   useEffect(() => {
     const fetchdata = async () => {
       const result = await axios(
-        "https://hn.algolia.com/api/v1/search?query=redux"
+        `https://hn.algolia.com/api/v1/search?query=${query}`
       );
       setData(result.data);
     };
@@ -16,16 +17,23 @@ function StateFetchData() {
     //   "https://hn.algolia.com/api/v1/search?query=redux"
     // );
     // setData(result.data);
-  }, []);
+  }, [query]);
 
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
